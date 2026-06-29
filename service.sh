22fe6c4d116135a2325200eaf0082126f32dc3ab
gaining <gaining7@outlook.com>
2026-06-28 20:43:15 -0400

    replace infinite mount polling with one-time boot retry

--- service.sh
+++ service.sh
@@ -2,17 +2,14 @@
 
 MODDIR=${0%/*}
 
-. "$MODDIR/common/viofo.sh"
-
-for i in $(seq 1 30); do
-    [ -d "$SRC" ] || {
-        sleep 2
-        continue
-    }
-
-    mount_viofo
-
-    mountpoint -q "$DST" && exit 0
-
+# Wait until internal storage is ready
+until [ -d /data/media/0 ]; do
     sleep 2
 done
+
+# Give MediaProvider a few extra seconds
+sleep 10
+
+. "$MODDIR/common/viofo.sh"
+
+mount_viofo
